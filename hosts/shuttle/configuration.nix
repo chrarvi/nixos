5705e1034ee92944132fdac631df0f9508fc22ee
christoffer arvidsson <christoffer@arvidson.nu>
2025-05-01 13:13:01 +0200

    Format

--- hosts/shuttle/configuration.nix
+++ hosts/shuttle/configuration.nix
@@ -5,228 +5,235 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+    imports =
+        [ # Include the results of the hardware scan.
+            ./hardware-configuration.nix
+        ];
+
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+    # Use the systemd-boot EFI boot loader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.grub.device = "nodev";
+    boot.loader.grub.efiSupport = true;
+
+    hardware.bluetooth.enable = true;
+    hardware.bluetooth.powerOnBoot = true;
+
+    networking.hostName = "shuttle";
+    networking.networkmanager.enable = true;
+
+    # Set your time zone.
+    time.timeZone = "Europe/Stockholm";
+
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
+    console = {
+        font = "Lat2-Terminus16";
+        useXkbConfig = true; # use xkb.options in tty.
+    };
+
+    # Enable the X11 windowing system.
+    services.xserver = {
+        enable = true;
+        dpi = 90;
+        xkb = {
+            layout = "us";
+        }; 
+        autoRepeatDelay = 250;
+        autoRepeatInterval = 30;
+        windowManager.bspwm.enable = true;
+    };
+    # HiDPI
+    environment.variables = {
+        GDK_SCALE = "2";
+        GDK_DPI_SCALE = "0.5";
+        _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
+    };
+
+    services.displayManager.ly = {
+        enable = true;
+    };
+
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
+
+    # Enable sound.
+    services.pipewire = {
+        enable = true;
+        pulse.enable = true;
+    };
+
+    # Enable touchpad support (enabled default in most desktopManager).
+    services.libinput.enable = true;
+
+    services.blueman.enable = true;
+
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.eethern = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+        packages = with pkgs; [
+            firefox
+            tree
+        ];
+    };
+
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+        lemurs 
+
+        vim 
+        inputs.neovim-nightly.packages.${system}.default
+
+        python3
+        pyright
+
+        fennel 
+        lua
+        luajit
+
+        acpi
+        alacritty
+        tmux
+        git
+        wget
+        xclip
+        stow
+        sqlite
+
+        cmake
+        gnumake 
+        gcc
+        autoconf
+        automake
+
+        polybar
+        zscroll
+        rofi
+        sxhkd
+
+        qutebrowser
+        firefox
+
+        bash
+        fish
+        fishPlugins.fzf-fish
+        fzf
+        starship
+        ranger
+        zoxide
+        ripgrep
+        fd
+        killall
+        jq
     ];
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.efiSupport = true;
 
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
-
-  networking.hostName = "shuttle";
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Stockholm";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    useXkbConfig = true; # use xkb.options in tty.
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-    dpi = 90;
-    xkb = {
-      layout = "us";
-    }; 
-    autoRepeatDelay = 250;
-    autoRepeatInterval = 30;
-    windowManager.bspwm.enable = true;
-  };
-  # HiDPI
-  environment.variables = {
-    GDK_SCALE = "2";
-    GDK_DPI_SCALE = "0.5";
-    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
-  };
-
-  services.displayManager.defaultSession = "none+bspwm";
- 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound.
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.libinput.enable = true;
-
-  services.blueman.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.eethern = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      firefox
-      tree
-    ];
-  };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim 
-    inputs.neovim-nightly.packages.${system}.default
-    fennel 
-    lua
-    luajit
-    python3
-
-    acpi
-    alacritty
-    tmux
-    git
-    wget
-    xclip
-    stow
-    sqlite
-
-    cmake
-    gnumake 
-    gcc
-    autoconf
-    automake
-
-    polybar
-    zscroll
-    rofi
-    sxhkd
-
-    qutebrowser
-    firefox
-
-    bash
-    fish
-    fishPlugins.fzf-fish
-    fzf
-    starship
-    ranger
-    zoxide
-    ripgrep
-    fd
-    killall
-    jq
-  ];
-
-    
-
-  # fish
-  programs.bash = {
-    interactiveShellInit = ''
+    # fish
+    programs.bash = {
+        interactiveShellInit = ''
       if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
       then
       shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
         exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
       fi
-    '';
-  };
-
-  # tmux
-  programs.tmux = {
-    enable = true;
-    plugins = with pkgs; [
-      tmuxPlugins.sensible
-      tmuxPlugins.continuum
-      tmuxPlugins.prefix-highlight
-      tmuxPlugins.yank
-      tmuxPlugins.rose-pine
-    ];
-  };
-
-  programs.git = {
-    enable = true;
-    config = {
-      user = {
-        name = "Christoffer Arvidsson";
-        email = "christoffer@arvidson.nu";
-      };
-      core.editor = "vim";
+        '';
     };
-  };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    settings = {
-      PasswordAuthentication = false;
+    # tmux
+    programs.tmux = {
+        enable = true;
+        plugins = with pkgs; [
+            tmuxPlugins.sensible
+            tmuxPlugins.continuum
+            tmuxPlugins.prefix-highlight
+            tmuxPlugins.yank
+            tmuxPlugins.rose-pine
+        ];
     };
-  };
 
-  # Fonts
-  fonts = {
-    packages = with pkgs; [       
-      iosevka                     
-      font-awesome
-      material-icons
-      (nerdfonts.override { fonts = [ "Iosevka" ]; })
-    ];                            
-    fontconfig = {
-      defaultFonts = {
-        monospace = [ "Iosevka Nerd Font Mono" ];
-      };
+    programs.git = {
+        enable = true;
+        config = {
+            user = {
+                name = "Christoffer Arvidsson";
+                email = "christoffer@arvidson.nu";
+            };
+            core.editor = "vim";
+        };
     };
-  };
+
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    programs.mtr.enable = true;
+    programs.gnupg.agent = {
+        enable = true;
+        enableSSHSupport = true;
+    };
+
+    # List services that you want to enable:
+
+    # Enable the OpenSSH daemon.
+    services.openssh = {
+        enable = true;
+        settings = {
+            PasswordAuthentication = false;
+        };
+    };
+
+    # Fonts
+    fonts = {
+        packages = with pkgs; [       
+            iosevka                     
+            font-awesome
+            material-icons
+            (nerdfonts.override { fonts = [ "Iosevka" ]; })
+        ];                            
+        fontconfig = {
+            defaultFonts = {
+                monospace = [ "Iosevka Nerd Font Mono" ];
+            };
+        };
+    };
 
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
+    # Copy the NixOS configuration file and link it from the resulting system
+    # (/run/current-system/configuration.nix). This is useful in case you
+    # accidentally delete configuration.nix.
+    # system.copySystemConfiguration = true;
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.05"; # Did you read the comment?
+    # This option defines the first version of NixOS you have installed on this particular machine,
+    # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+    #
+    # Most users should NEVER change this value after the initial install, for any reason,
+    # even if you've upgraded your system to a new NixOS release.
+    #
+    # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+    # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+    # to actually do that.
+    #
+    # This value being lower than the current NixOS release does NOT mean your system is
+    # out of date, out of support, or vulnerable.
+    #
+    # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+    # and migrated your data accordingly.
+    #
+    # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+    system.stateVersion = "24.05"; # Did you read the comment?
 
 }
 
